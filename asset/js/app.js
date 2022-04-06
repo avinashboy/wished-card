6a1bfde39fffca5453fe788229d51c63338a46f7
avinashboy <52669990+avinashboy@users.noreply.github.com>
2022-04-06 06:05:38 +0200

    Add files via upload

--- asset/js/app.js
+++ asset/js/app.js
@@ -23,45 +23,53 @@ function fontFamilyListFun() {
 }
 fontFamilyListFun();
 
+function addSpaceText({string}) {
+  console.log('string:', string)
+  const addSpace = Math.floor((12 - string.length) / 2)
+  console.log('addSpace:', addSpace)
+  return `${Array(addSpace).fill('\xa0').join('')}${string}${Array(addSpace).fill('\xa0').join('')}`
+}
+
 function wished() {
-  console.log("okay");
   // getting the data
 
   nameInput = $("#nameInput");
-  console.log("nameInput:", nameInput);
   fontFamily = $("#fontFamily");
 
   // validate
   //   if(validator.isEmpty(nameInput.val())) return alertUser(`Please enter a name`)
 
-  createCanvas({
-    name: nameInput.val(),
+   createCanvas({
+    name: addSpaceText({string: nameInput.val()}),
     fontFamily: fontFamily.val(),
   });
+
+  // const gg = addSpaceText({string: nameInput.val()})
+  // console.log('gg:', gg)
 
   console.log("end");
 }
 
-async function createCanvas({ name, fontFamily }) {
+function createCanvas({ name, fontFamily }) {
   const meta = data[fontFamily];
-
   const image = new Image();
-  image.src = `./asset/image/${fontFamily}.png`;
+  image.src = meta.url;
+  image.crossOrigin="anonymous"
   image.onload = function () {
     $(".viewImage").empty();
     canvasElement.width = this.width;
     canvasElement.height = this.height;
     const ctx = canvasElement.getContext("2d");
     ctx.drawImage(image, 0, 0, canvasElement.width, canvasElement.height);
-    const text = `${meta.fontWeight} ${meta.fontSize || 20}px ${meta.fontFamily}`;
+    const text = `${meta.fontWeight} ${meta.fontSize}px ${meta.fontFamily}`;
     console.log("text:", text);
     ctx.fillStyle = `${meta.color}`;
     ctx.font = text;
-    ctx.fillText(name, meta.postionX || 20, meta.postionY || 25);
+    ctx.fillText(name, meta.postionX, meta.postionY);
     const showImage = document.createElement("img");
     showImage.src = canvasElement.toDataURL("image/png");
-    showImage.width = canvasElement.width / 3;
-    showImage.height = canvasElement.height / 3;
+    showImage.width = canvasElement.width / 1.5;
+    showImage.height = canvasElement.height / 1.5;
     $(".viewImage").append(showImage);
     // document.getElementById("gg").style.fontFamily = meta.fontFamily
   };
--- asset/js/data.js
+++ asset/js/data.js
@@ -1,42 +1,47 @@
 const data = {
     "happy birthday (Flyer)":{
       color: "#796F45",
-      fontFamily: "adam script, cursive",
-      postionX: 130,
-      postionY: 1530,
-      fontSize: 200,
+      fontFamily: "adam script",
+      postionX: 127,
+      postionY: 700,
+      fontSize: 100,
       fontWeight: "bold",
+      url: "https://i.ibb.co/gZYPYBn/happy-birthday-Flyer.png"
     },
     "happy Birthday (Poster)":{
       color: "#A67D2E",
       fontFamily: "'Oregano', cursive",
-      postionX: 150,
-      postionY: 1250,
-      fontSize:280,
+      postionX: 110,
+      postionY: 500,
+      fontSize:100,
       fontWeight: "normal",
+      url: "https://i.ibb.co/9r3JMwB/happy-Birthday-Poster.png" 
     },
     "Pastel Cute Playful Happy Birthday Flyers Landscape A4":{
       color: "#2C3037",
       fontFamily: "bellaboo",
-      postionX: 580,
-      postionY: 900,
-      fontSize:184,
+      postionX: 110,
+      postionY: 280,
+      fontSize:80,
       fontWeight: "bold",
+      url: "https://i.ibb.co/Jc1rSxH/Pastel-Cute-Playful-Happy-Birthday-Flyers-Landscape-A4.png" 
     },
     "happy birthday instagram post":{
       color: "#87879E",
       fontFamily: "'Yeseva One', cursive",
-      postionX: 220,
-      postionY: 750,
-      fontSize: 100,
+      postionX: 120,
+      postionY: 450,
+      fontSize: 60,
       fontWeight: "normal",
+      url: "https://i.ibb.co/RTQkTJd/happy-birthday-instagram-post.png"
     },
     "Yellow Border Happy Birthday Poster":{
       color: "#000000",
       fontFamily: "'Mr Dafoe', cursive",
-      postionX: 320,
-      postionY: 850,
-      fontSize: 250,
+      postionX: 93,
+      postionY: 350,
+      fontSize: 110,
       fontWeight: "normal",
+      url: "https://i.ibb.co/p31P0vy/Yellow-Border-Happy-Birthday-Poster.png"
     },
   }
