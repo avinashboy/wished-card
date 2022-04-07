e3cbb4d33ba9f22b96626d84b401d3aa78a86f19
avinashboy <52669990+avinashboy@users.noreply.github.com>
2022-04-07 06:26:02 +0200

    Add files via upload

---
+++ wished card/asset/js/data.js
@@ -0,0 +1,57 @@
+const data = {
+  "happy Birthday (Poster)": {
+    color: "#A67D2E",
+    fontName: "Oregano",
+    fontFamily: "'Oregano', cursive",
+    postionX: 110,
+    postionY: 500,
+    fontSize: 100,
+    fontWeight: "normal",
+    url: "https://i.ibb.co/9r3JMwB/happy-Birthday-Poster.png",
+    fontUrl:"https://raw.githubusercontent.com/avinashboy/demos/main/asset/fonts/Oregano-Regular.ttf"
+  },
+  "happy birthday instagram post": {
+    color: "#87879E",
+    fontName: "Yeseva One",
+    fontFamily: "'Yeseva One', cursive",
+    postionX: 120,
+    postionY: 450,
+    fontSize: 60,
+    fontWeight: "normal",
+    url: "https://i.ibb.co/RTQkTJd/happy-birthday-instagram-post.png",
+    fontUrl: "https://raw.githubusercontent.com/avinashboy/demos/main/asset/fonts/YesevaOne-Regular.ttf"
+  },
+  "Yellow Border Happy Birthday Poster": {
+    color: "#000000",
+    fontName: "Mr Dafoe",
+    fontFamily: "'Mr Dafoe', cursive",
+    postionX: 93,
+    postionY: 350,
+    fontSize: 110,
+    fontWeight: "normal",
+    url: "https://i.ibb.co/p31P0vy/Yellow-Border-Happy-Birthday-Poster.png",
+    fontUrl: "https://raw.githubusercontent.com/avinashboy/demos/main/asset/fonts/MrDafoe-Regular.ttf"
+  },
+  "happy birthday (Flyer)": {
+    color: "#796F45",
+    fontName: "adam script",
+    fontFamily: "adam script",
+    postionX: 127,
+    postionY: 700,
+    fontSize: 100,
+    fontWeight: "bold",
+    url: "https://i.ibb.co/gZYPYBn/happy-birthday-Flyer.png",
+    fontUrl: "https://raw.githubusercontent.com/avinashboy/demos/main/asset/fonts/Adam-Script.ttf"
+  },
+  "Pastel Cute Playful Happy Birthday Flyers Landscape A4": {
+    color: "#2C3037",
+    fontName: "bellaboo",
+    fontFamily: "bellaboo",
+    postionX: 110,
+    postionY: 280,
+    fontSize: 80,
+    fontWeight: "bold",
+    url: "https://i.ibb.co/Jc1rSxH/Pastel-Cute-Playful-Happy-Birthday-Flyers-Landscape-A4.png",
+    fontUrl: "https://raw.githubusercontent.com/avinashboy/demos/main/asset/fonts/BELLABOO-Regular.ttf"
+  },
+};
